862227e6fe2eae28dd1ff5e20623f60b9ce3758b
rajnisingh2026 <06393rj@gmail.com>
2025-02-14 13:49:46 +0530

    Create mergeKSortedLists.cpp

---
+++ mergeKSortedLists.cpp
@@ -0,0 +1,25 @@
+#include <queue>
+class Solution {
+public:
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
+        for (ListNode* list : lists) {
+            if (list) minHeap.push(list);
+        }
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (!minHeap.empty()) {
+            ListNode* smallest = minHeap.top();
+            minHeap.pop();
+            tail->next = smallest;
+            tail = tail->next;
+            if (smallest->next) minHeap.push(smallest->next);
+        }
+        return dummy.next;
+    }
+};
