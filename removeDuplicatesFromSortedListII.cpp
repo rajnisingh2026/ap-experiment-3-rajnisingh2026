b49cced3806b2a8e27101555fbd8924f89cffac9
rajnisingh2026 <06393rj@gmail.com>
2025-02-14 13:44:50 +0530

    Create removeDuplicatesFromSortedListII.cpp

---
+++ removeDuplicatesFromSortedListII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+        ListNode dummy(0); 
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        while (head) {
+            if (head->next && head->val == head->next->val) {
+                while (head->next && head->val == head->next->val) {
+                    head = head->next;
+                }
+                prev->next = head->next;
+            } else {
+                prev = prev->next;
+            }
+            head = head->next; 
+        }
+        return dummy.next;
+    }
+};
