daef3e306e88fb77966e3e859ce021ec9b44b1df
rajnisingh2026 <06393rj@gmail.com>
2025-02-14 12:50:26 +0530

    Create printLinkedList.cpp

---
+++ printLinkedList.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void printList(Node *head) {
+        Node* temp = head;
+        while(temp != nullptr){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+};
